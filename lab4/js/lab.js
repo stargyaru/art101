0b9c9eb4584885aac72a9196f560cb852718538c
Annalivia <threeannathree@gmail.com>
2022-04-20 09:45:07 -0700

    lab 5 things!

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author annalivia and nathan.
+// @since  4.18.22
+
+//retrieve cup
+//retrieve eggs
+//retrieve fork
+//crack eggs (how many?) into cup
+    //stir with fork until blended
+//turn heat on pan
+//place butter into pan
+    //wait for butter to melt
+//place eggs from cup into pan
+    //wait until they are no longer liquid
+//remove from pan
---
+++ lab5/js/lab.js
@@ -0,0 +1,25 @@
+// Summary. (Lab 5 by Jackie and Annalivia)
+// Description. (Calculating the make and year of Car)
+// @author jackie and annalivia
+// @since  4.20.22
+
+//define variables
+var make = "Kia";
+var model = "Seltos";
+var color = "Steel Grey";
+var year = 2020;
+var currentYear = 2022;
+
+//subtract from to calculate car year
+var carAge = currentYear - year;
+
+//boolean
+let ownIt = true
+
+//output
+document.writeln("Make: "+make + "<br>")
+document.writeln("Model: "+model + "<br>")
+document.writeln("Color: "+color + "<br>")
+document.writeln("Year: "+year + "<br>")
+document.writeln("Age: "+carAge + "<br>")
+document.writeln("Own It?: "+ownIt + "<br>")
