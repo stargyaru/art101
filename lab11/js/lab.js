3560fefe3fcfb76c7b9f042db3a569e5a82c046b
Annalivia <threeannathree@gmail.com>
2022-05-15 22:26:33 -0700

    random commits lol

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,29 +1,32 @@
+/*Author: Annalivia Martin-Straw
+*Created: 05.011.2022
+*License: Public Domain
 /*Author: Tamara Basbuyuk
-*Created: 05.01.2022
+*Created: 05.11.2022
 *License: Public Domain
 *
 */
-//react.js
-<script
-			  src="https://code.jquery.com/jquery-3.6.0.min.js"
-			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-			  crossorigin="anonymous"></script>
 
+//button that alerts user in challenges section
+var challengeEL=$("challenge");
+var button1= $(challengeEL).append(<button>hi</button>);
 
+//button that alerts user in problems section
+$('#problems').append(
+    $(document.createElement('button')).prop({
+        type: 'button',
+        innerHTML: 'Press me',
+        class: 'btn-styled',
+	click: function () { alert('hi this is problems'); }
+	})
+);
 
-var outputEI = document.getElementById("output");
-var new1EI = document.createElement("p");
-var new2EI = document.createElement("h2");
-console.log(outputEI);
-new1EI.innerHTML ="hello";
-outputEI.appendChild(new1EI);
-outputEI.appendChild(new2EI);
-
-
-
-//modifying the css through DOM elements (slide 28)
-outputEI.style.color = "blue";
-
-document.getElementById("lab9summary").style.marginLeft = "50px";
-
-document.getElementById("lab9summary").style.position = "absolute";
+//button that alerts user in results section
+$('#results').append(
+    $(document.createElement('button')).prop({
+        type: 'button',
+        innerHTML: 'Press me',
+        class: 'btn-styled',
+		click: function () { alert('hi this is results'); }
+	})
+);
